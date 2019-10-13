85e3e5817724c04138d3f534673fefd9ce4ca947
PURUSHOTTAM <purushottamn8@gmail.com>
2019-10-13 22:49:46 +0530

    driver updater for mysql JDBC

--- studentdata/src/main/java/com/decipherzone/config/DBconfig.java
+++ studentdata/src/main/java/com/decipherzone/config/DBconfig.java
@@ -9,7 +9,7 @@ public class DBconfig {
     public static Connection getConnection() {
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/data?useSSL=false", "root", "root");
         } catch (Exception e) {
